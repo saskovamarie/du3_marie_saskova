96f8579abf1f49b481b7ca048b4729468d14ab02
Marie Šašková <saskovamarie11@gmail.com>
2020-02-04 20:23:14 +0100

    ošetření výjimek

--- ukol3.py
+++ ukol3.py
@@ -1,6 +1,6 @@
 import pathlib, json, sys
 
-cesta = pathlib.Path('C:/Users/Marie Šašková/Desktop/du3_testdata')
+cesta = pathlib.Path('C:/Users/Marie Šašková/Desktop/marie_ukol')
 
 # vypsat co je v adresáři
 
@@ -16,17 +16,15 @@ def iter_podadresar(filepath, points_list, lines_list, polygons_list):
                             data = json.load(f)
                             features = data["features"]
 
-
                             if len(features) == 0:
-                                print("prázdný json")
+                                print("Error: soubor:" , filepath," je prázdný!")
                                 continue
 
                             for fts in features:
                                 try:
                                     fts["properties"]["path"] = str(files)
-
                                     geom_type = (fts["geometry"]["type"])
-                                    #print(geom_type)
+
                                     if geom_type in ("MultiPoint","Point"):
                                         points_list.append(fts)
                                     elif geom_type in ("MultiLineString", "LineString"):
@@ -34,26 +32,27 @@ def iter_podadresar(filepath, points_list, lines_list, polygons_list):
                                     elif geom_type in ("MultiPolygon", "Polygon"):
                                         polygons_list.append(fts)
                                     else:
-                                        print("nic")
+                                        print("Error: v souboru: ", files, " se objevuje jiný typ objektu!")
                                 except KeyError:
-                                    print("chybná geometrie")
+                                    print("Error: v souboru:" ,files, " se objevuje chybná geometrie!")
 
                 elif files.is_dir():
                     iter_podadresar(files, points_list, lines_list, polygons_list)
-                    #print(files)
+
             except ValueError:
-                print("vadný json")
+                print("Error: soubor geoJSON:", files, " je poškozený! ")
+
     except FileNotFoundError:
-        print("chybná cesta")
+        print("Error: zadaná cesta k souboru: ", files, " je chybná!")
 
 points = []
 lines = []
 polygons = []
 iter_podadresar(cesta, points,lines, polygons)
+
 # tvorba json - points
 gj_structure_points = {"type": "FeatureCollection"}
 gj_structure_points['features'] = points
-
 # zapis souboru
 with open("points.geojson", "w", encoding="utf-8") as f:
     json.dump(gj_structure_points, f, indent=2)
@@ -62,7 +61,6 @@ with open("points.geojson", "w", encoding="utf-8") as f:
 # tvorba json - lines
 gj_structure_lines = {"type": "FeatureCollection"}
 gj_structure_lines['features'] = lines
-
 # zapis souboru
 with open("lines.geojson", "w", encoding="utf-8") as f:
     json.dump(gj_structure_lines, f, indent=2)
@@ -71,7 +69,6 @@ with open("lines.geojson", "w", encoding="utf-8") as f:
 # tvorba json - polygons
 gj_structure_polygons = {"type": "FeatureCollection"}
 gj_structure_polygons['features'] = polygons
-
 # zapis souboru
 with open("polygons.geojson", "w", encoding="utf-8") as f:
     json.dump(gj_structure_polygons, f, indent=2)
