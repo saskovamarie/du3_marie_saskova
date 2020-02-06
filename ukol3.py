8ef25142d1939f45ff7eaae620a9d69e2132da02
Marie Šašková <saskovamarie11@gmail.com>
2020-02-06 19:32:09 +0100

    doplnění komentářů

--- ukol3.py
+++ ukol3.py
@@ -1,28 +1,31 @@
 import pathlib, json, sys
 
-cesta = pathlib.Path('C:/Users/Marie Šašková/Desktop/marie_ukol')
-
-# vypsat co je v adresáři
-
-def iter_podadresar(filepath, points_list, lines_list, polygons_list):
+def split_json(filepath, points_list, lines_list, polygons_list):
+    """
+    funkce projde všechny soubory v zadaném adresáři a jejo podadresářích, vybere všechny soubory geoJSON a podle typu geometrie
+    rozdělí objekty do seznamů, zároveň ošetřuje výjimky, které mohou nastat
+    :param filepath: cesta k vstupnímu adresáři
+    :param points_list: seznam bodů
+    :param lines_list: seznam linií
+    :param polygons_list: seznam polygonů
+    """
     try:
-        adir = filepath
-        for files in adir.iterdir():
+        for files in filepath.iterdir():
             try:
                 if files.is_file():
                     if files.suffix in (".json", ".geojson"):
                         print(files)
-                        with files.open(mode="r", encoding="utf-8") as f:
+                        with files.open(mode="r", encoding="utf-8") as f:   # otevření souboru
                             data = json.load(f)
                             features = data["features"]
 
-                            if len(features) == 0:
-                                print("Error: soubor:" , filepath," je prázdný!")
+                            if len(features) == 0:   # ošetření případu, kdy seznam 'features' je prázdný
+                                print("Error: soubor:", files," neobsahuje žádné objekty!")
                                 continue
 
                             for fts in features:
                                 try:
-                                    fts["properties"]["path"] = str(files)
+                                    fts["properties"]["path"] = str(files)   # zapsání cesty k původnímu souboru do properties
                                     geom_type = (fts["geometry"]["type"])
 
                                     if geom_type in ("MultiPoint","Point"):
@@ -32,43 +35,49 @@ def iter_podadresar(filepath, points_list, lines_list, polygons_list):
                                     elif geom_type in ("MultiPolygon", "Polygon"):
                                         polygons_list.append(fts)
                                     else:
-                                        print("Error: v souboru: ", files, " se objevuje jiný typ objektu!")
+                                        print("v souboru: ", files, " se objevuje jiný typ objektu!")
                                 except KeyError:
                                     print("Error: v souboru:" ,files, " se objevuje chybná geometrie!")
 
                 elif files.is_dir():
-                    iter_podadresar(files, points_list, lines_list, polygons_list)
+                    split_json(files, points_list, lines_list, polygons_list)   # rekurzivní volání funkce
 
             except ValueError:
                 print("Error: soubor geoJSON:", files, " je poškozený! ")
 
     except FileNotFoundError:
-        print("Error: zadaná cesta k souboru: ", files, " je chybná!")
+        print("Error: soubor ", filepath, " neexistuje!")
+
+
 
 points = []
 lines = []
 polygons = []
-iter_podadresar(cesta, points,lines, polygons)
 
-# tvorba json - points
-gj_structure_points = {"type": "FeatureCollection"}
-gj_structure_points['features'] = points
-# zapis souboru
-with open("points.geojson", "w", encoding="utf-8") as f:
-    json.dump(gj_structure_points, f, indent=2)
+cesta = pathlib.Path(sys.argv[1])
+split_json(cesta, points,lines, polygons)
 
+try:
+    # tvorba json - points
+    gj_structure_points = {"type": "FeatureCollection"}
+    gj_structure_points['features'] = points
+    # zapis souboru
+    with open("points.geojson", "w", encoding="utf-8") as f:
+        json.dump(gj_structure_points, f, indent=2,  ensure_ascii=False)
 
-# tvorba json - lines
-gj_structure_lines = {"type": "FeatureCollection"}
-gj_structure_lines['features'] = lines
-# zapis souboru
-with open("lines.geojson", "w", encoding="utf-8") as f:
-    json.dump(gj_structure_lines, f, indent=2)
+    # tvorba json - lines
+    gj_structure_lines = {"type": "FeatureCollection"}
+    gj_structure_lines['features'] = lines
+    # zapis souboru
+    with open("lines.geojson", "w", encoding="utf-8") as f:
+        json.dump(gj_structure_lines, f, indent=2, ensure_ascii=False)
 
+    # tvorba json - polygons
+    gj_structure_polygons = {"type": "FeatureCollection"}
+    gj_structure_polygons['features'] = polygons
+    # zapis souboru
+    with open("polygons.geojson", "w", encoding="utf-8") as f:
+        json.dump(gj_structure_polygons, f, indent=2, ensure_ascii=False)
 
-# tvorba json - polygons
-gj_structure_polygons = {"type": "FeatureCollection"}
-gj_structure_polygons['features'] = polygons
-# zapis souboru
-with open("polygons.geojson", "w", encoding="utf-8") as f:
-    json.dump(gj_structure_polygons, f, indent=2)
+except PermissionError:
+    print("Error: v daném adresáři nemáte oprávnění vytvořit nový soubor")
