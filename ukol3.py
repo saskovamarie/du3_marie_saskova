0c975057f0eb5a1cab465cc7f91e75842610bdb5
Marie Šašková <saskovamarie11@gmail.com>
2020-02-10 19:48:37 +0100

    oprava issues

--- ukol3.py
+++ ukol3.py
@@ -9,44 +9,41 @@ def split_json(filepath, points_list, lines_list, polygons_list):
     :param lines_list: seznam linií
     :param polygons_list: seznam polygonů
     """
-    try:
-        for files in filepath.iterdir():
-            try:
-                if files.is_file():
-                    if files.suffix in (".json", ".geojson"):
-                        print(files)
-                        with files.open(mode="r", encoding="utf-8") as f:   # otevření souboru
-                            data = json.load(f)
-                            features = data["features"]
+    for file in filepath.iterdir():
+        try:
+            if file.is_file():
+                if file.suffix in (".json", ".geojson"):
+                    print(file)
+                    with file.open(mode="r", encoding="utf-8") as f:   # otevření souboru
+                        data = json.load(f)
+                        features = data["features"]
 
-                            if len(features) == 0:   # ošetření případu, kdy seznam 'features' je prázdný
-                                print("Error: soubor:", files," neobsahuje objekty!")
-                                continue
+                        if len(features) == 0:   # ošetření případu, kdy seznam 'features' je prázdný
+                            print("Error: soubor:", file," neobsahuje objekty!")
+                            continue
 
-                            for fts in features:
-                                try:
-                                    fts["properties"]["path"] = str(files)   # zapsání cesty k původnímu souboru do properties
-                                    geom_type = (fts["geometry"]["type"])
+                        for ft in features:
+                            try:
+                                ft["properties"]["filepath"] = str(file)   # zapsání cesty k původnímu souboru do properties
+                                geom_type = (ft["geometry"]["type"])
 
-                                    if geom_type in ("MultiPoint","Point"):
-                                        points_list.append(fts)
-                                    elif geom_type in ("MultiLineString", "LineString"):
-                                        lines_list.append(fts)
-                                    elif geom_type in ("MultiPolygon", "Polygon"):
-                                        polygons_list.append(fts)
-                                    else:
-                                        print("v souboru: ", files, " se objevuje jiný typ objektu!")
-                                except KeyError:
-                                    print("Error: v souboru:" ,files, " se objevuje chybná geometrie!")
+                                if geom_type in ("MultiPoint","Point"):
+                                    points_list.append(ft)
+                                elif geom_type in ("MultiLineString", "LineString"):
+                                    lines_list.append(ft)
+                                elif geom_type in ("MultiPolygon", "Polygon"):
+                                    polygons_list.append(ft)
+                                else:
+                                    print("v souboru: ", file, " se objevuje jiný typ objektu:", geom_type)
+                            except KeyError:
+                                print("Error: v souboru:" ,file, " se objevuje chybná geometrie!")
 
-                elif files.is_dir():
-                    split_json(files, points_list, lines_list, polygons_list)   # rekurzivní volání funkce
+            elif file.is_dir():
+                split_json(file, points_list, lines_list, polygons_list)   # rekurzivní volání funkce
 
-            except ValueError:
-                print("Error: soubor:", files, " je poškozený! ")
+        except ValueError:
+            print("Error: soubor:", file, " je poškozený! ")
 
-    except FileNotFoundError:
-        print("Error: adresář ", filepath, " neexistuje!")
 
 
 
@@ -54,9 +51,13 @@ points = []
 lines = []
 polygons = []
 
-cesta = pathlib.Path(sys.argv[1])
+try:
+    cesta = pathlib.Path(sys.argv[1])
+    split_json(cesta, points, lines, polygons)
+except FileNotFoundError:
+    print("Error: adresář ", cesta, " neexistuje!")
 
-split_json(cesta, points,lines, polygons)
+
 try:
     # tvorba json - points
     gj_structure_points = {"type": "FeatureCollection"}
@@ -64,20 +65,25 @@ try:
     # zapis souboru
     with open("points.geojson", "w", encoding="utf-8") as f:
         json.dump(gj_structure_points, f, indent=2,  ensure_ascii=False)
+except PermissionError:
+    print("Error: v daném adresáři nemáte oprávnění vytvořit nový soubor")
 
+try:
     # tvorba json - lines
     gj_structure_lines = {"type": "FeatureCollection"}
     gj_structure_lines['features'] = lines
     # zapis souboru
     with open("lines.geojson", "w", encoding="utf-8") as f:
         json.dump(gj_structure_lines, f, indent=2, ensure_ascii=False)
+except PermissionError:
+    print("Error: v daném adresáři nemáte oprávnění vytvořit nový soubor")
 
+try:
     # tvorba json - polygons
     gj_structure_polygons = {"type": "FeatureCollection"}
     gj_structure_polygons['features'] = polygons
     # zapis souboru
     with open("polygons.geojson", "w", encoding="utf-8") as f:
         json.dump(gj_structure_polygons, f, indent=2, ensure_ascii=False)
-
 except PermissionError:
     print("Error: v daném adresáři nemáte oprávnění vytvořit nový soubor")
